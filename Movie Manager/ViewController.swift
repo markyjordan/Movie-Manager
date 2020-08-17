036a63ec1eb5fd3ab042f914b7af01dbc5d8355b
Mark Jordan <markybjordan@gmail.com>
2020-08-17 16:23:58 -0700

    Created a new project and added the defoult files to the repo.

---
+++ Movie Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/17/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
