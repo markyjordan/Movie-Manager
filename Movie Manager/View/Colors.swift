76f09425698cb917deec8548798098181efe2d9c
Mark Jordan <markybjordan@gmail.com>
2020-08-18 16:10:41 -0700

    Extended UIColor class with static color properties.

---
+++ Movie Manager/View/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let primaryDark = UIColor(named: "PrimaryDark")!
+    static let primaryLight = UIColor(named: "PrimaryLight")!
+    static let gradientTop = UIColor(named: "GradientTop")!
+    static let gradientBottom = UIColor(named: "GradientBottom")!
+}
