bc9ca19559b4ccd973760d35978d473724a235da
Mark Jordan <markybjordan@gmail.com>
2020-08-18 16:03:10 -0700

    Created a model object for json movie results data.

---
+++ Movie Manager/Model/TMDB Responses/MovieResults.swift
@@ -0,0 +1,24 @@
+//
+//  MovieResults.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+struct MovieResults: Codable {
+    
+    let page: Int
+    let results: [Movie]
+    let totalPages: Int
+    let totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
