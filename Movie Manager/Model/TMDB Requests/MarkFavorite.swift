31a898d781f86646e05c776bd115d23b0d03763a
Mark Jordan <markybjordan@gmail.com>
2020-08-18 15:52:32 -0700

    Defined a class for adding movie favorites.

---
+++ Movie Manager/Model/TMDB Requests/MarkFavorite.swift
@@ -0,0 +1,14 @@
+//
+//  MarkFavorite.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+class MarkFavorite {
+    
+    
+}
