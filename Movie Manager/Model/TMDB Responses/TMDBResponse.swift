6aa42325c58058f18c77b82254b4b5f7695cb5eb
Mark Jordan <markybjordan@gmail.com>
2020-08-18 16:06:48 -0700

    Created a model object for TMDB response data.

---
+++ Movie Manager/Model/TMDB Responses/TMDBResponse.swift
@@ -0,0 +1,14 @@
+//
+//  TMDBResponse.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+struct TMDBResponse: Codable {
+    
+    
+}
