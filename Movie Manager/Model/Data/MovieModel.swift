807e2c559bcb0deeeec03e58b37ae4081608f283
Mark Jordan <markybjordan@gmail.com>
2020-08-18 14:45:41 -0700

    Created a MovieModel class.
    The data model includes properties for a movie watchlist and favorites list.

---
+++ Movie Manager/Model/Data/MovieModel.swift
@@ -0,0 +1,15 @@
+//
+//  MovieModel.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+class MovieModel {
+    
+    static var watchList = [Movie]()
+    static var favorites = [Movie]()
+}
