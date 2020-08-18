a5dccd874d04025fb703aeea5730b8e8b3c5294b
Mark Jordan <markybjordan@gmail.com>
2020-08-18 14:49:12 -0700

    Added a network client interaction class.
    Included a static property to hold the user's API key.

---
+++ Movie Manager/Model/TMDB Client/TMDBClient.swift
@@ -0,0 +1,14 @@
+//
+//  TMDBClient.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+class TMDBClient {
+    
+    static let apiKey = "YOUR_TMDB_API_KEY"
+}
