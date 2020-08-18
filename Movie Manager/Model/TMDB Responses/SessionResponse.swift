747ad2c3a1c5300c2c27b1d8c54b83c12ea7dab6
Mark Jordan <markybjordan@gmail.com>
2020-08-18 16:08:10 -0700

    Created a model object for session response data.

---
+++ Movie Manager/Model/TMDB Responses/SessionResponse.swift
@@ -0,0 +1,14 @@
+//
+//  SessionResponse.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+struct SessionResponse: Codable {
+    
+    
+}
