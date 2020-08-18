c6fc1d96e94dc2a55d8960f815dbaeca21630db0
Mark Jordan <markybjordan@gmail.com>
2020-08-18 15:10:20 -0700

    Created an enum to distinguish media types.
    Media type cases include movie and tv.

---
+++ Movie Manager/Model/TMDB Client/TMDBTypes.swift
@@ -0,0 +1,14 @@
+//
+//  TMDBTypes.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+enum MediaType: String, Codable {
+    
+    case movie, tv
+}
