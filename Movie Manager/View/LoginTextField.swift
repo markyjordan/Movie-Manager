803f3d252df51671f3d72b784667a6068dcfd679
Mark Jordan <markybjordan@gmail.com>
2020-08-18 19:17:33 -0700

    Defined a login text field class.

---
+++ Movie Manager/View/LoginTextField.swift
@@ -0,0 +1,33 @@
+//
+//  LoginTextField.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginTextField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 5
+        tintColor = UIColor.white
+        backgroundColor = UIColor.primaryLight
+
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        let insetBounds = CGRect(x: bounds.origin.x + 8, y: bounds.origin.y, width: bounds.size.width - 16, height: bounds.size.height)
+        return insetBounds
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let insetBounds = CGRect(x: bounds.origin.x + 8, y: bounds.origin.y, width: bounds.size.width - 16, height: bounds.size.height)
+        return insetBounds
+    }
+}
