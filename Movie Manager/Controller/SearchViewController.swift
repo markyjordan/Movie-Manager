8d8cf77c13fc45f9bcb79ae2d7f5204de4393bc2
Mark Jordan <markybjordan@gmail.com>
2020-08-21 17:52:31 -0700

    Defined SearchViewController class.

---
+++ Movie Manager/Controller/SearchViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SearchViewController.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/21/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [Movie]()
+    
+    var selectedIndex = 0
+    
+    // show detail view upon tap of a table view cell
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            let detailVC = segue.destination as! MovieDetailViewController
+            detailVC.movie = movies[selectedIndex]
+        }
+    }
+}
+
+// MARK:  UISearchBar Delegate Methods
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+}
+
+// MARK: - UITableView Data Source and Delegate Methods
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell")!
+        
+        let movie = movies[indexPath.row]
+        
+        cell.textLabel?.text = "\(movie.title) - \(movie.releaseYear)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+        performSegue(withIdentifier: "showDetail", sender: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
