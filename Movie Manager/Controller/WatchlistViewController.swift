886d97c299748137bf9b95a2adc1cde23a0b771a
Mark Jordan <markybjordan@gmail.com>
2020-08-21 18:02:40 -0700

    Defined WatchlistViewController class.

---
+++ Movie Manager/Controller/WatchlistViewController.swift
@@ -0,0 +1,66 @@
+//
+//  WatchlistViewController.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/21/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class WatchlistViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var selectedIndex = 0
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        _ = TMDBClient.getWatchlist() { movies, error in
+            MovieModel.watchlist = movies
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+}
+
+// MARK: - UITableView Delegate and Data Source Methods
+
+extension WatchlistViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MovieModel.watchlist.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell")!
+        
+        let movie = MovieModel.watchlist[indexPath.row]
+        
+        cell.textLabel?.text = movie.title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+        performSegue(withIdentifier: "showDetail", sender: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
