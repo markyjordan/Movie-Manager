296368665050213e1d8976c4bc3b07d13035da54
Mark Jordan <markybjordan@gmail.com>
2020-08-23 19:59:39 -0700

    Added Constants.swift to .gitignore.

---
+++ Movie Manager/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Movie Manager
+//
+//  Created by Marky Jordan on 8/23/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+let myTmdbApiKey: String = "98aacce3a82ea00687f71fac995140cf"
