3927c3f32d733dfa849a0b1835fe1b54ff7a5bc7
Mark Jordan <markybjordan@gmail.com>
2020-08-25 18:21:58 -0700

    Defined RequestTokenResponse.

--- Movie Manager/Model/TMDB Client/TMDBClient.swift
+++ Movie Manager/Model/TMDB Client/TMDBClient.swift
@@ -54,3 +54,4 @@ class TMDBClient {
         task.resume()
     }
 }
+
--- Movie Manager/Model/TMDB Responses/RequestTokenResponse.swift
+++ Movie Manager/Model/TMDB Responses/RequestTokenResponse.swift
@@ -10,5 +10,13 @@ import Foundation
 
 struct RequestTokenResponse: Codable {
     
+    let success: Bool
+    let expiresAt: String
+    let requestToken: String
     
+    enum CodingKeys: String, CodingKey {
+        case success = "success"
+        case expiresAt = "expires_at"
+        case requestToken = "request_token"
+    }
 }
