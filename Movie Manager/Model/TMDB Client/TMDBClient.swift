7b86e4077131ca0877b07447102328473bb171f1
Mark Jordan <markybjordan@gmail.com>
2020-10-05 10:17:56 -0700

    Reformatted the cases in 'Endpoints' enum.
    Reordered the cases to group similarities together.

--- Movie Manager/Model/TMDB Client/TMDBClient.swift
+++ Movie Manager/Model/TMDB Client/TMDBClient.swift
@@ -36,16 +36,16 @@ class TMDBClient {
                 return Endpoints.base + "/authentication/token/new" + Endpoints.apiKeyParam
             case .getWatchlist:
                 return Endpoints.base + "/account/\(Auth.accountId)/watchlist/movies" + Endpoints.apiKeyParam + "&session_id=\(Auth.sessionId)"
-            case .login:
-                return Endpoints.base + "/authentication/token/validate_with_login" + Endpoints.apiKeyParam
-            case .createSessionId:
-                return Endpoints.base + "/authentication/session/new" + Endpoints.apiKeyParam
-            case .webAuth:
-                return "https://www.themoviedb.org/authenticate/" + Auth.requestToken + "?redirect_to=themoviemanager:authenticate"
-            case .logout:
-                return Endpoints.base + "/authentication/session" + Endpoints.apiKeyParam
             case .getFavorites:
                 return Endpoints.base + "/account/\(Auth.accountId)/favorite/movies" + Endpoints.apiKeyParam + "&session_id=\(Auth.sessionId)"
+            case .login:
+                return Endpoints.base + "/authentication/token/validate_with_login" + Endpoints.apiKeyParam
+            case .webAuth:
+                return "https://www.themoviedb.org/authenticate/" + Auth.requestToken + "?redirect_to=themoviemanager:authenticate"
+            case .createSessionId:
+                return Endpoints.base + "/authentication/session/new" + Endpoints.apiKeyParam
+            case .logout:
+                return Endpoints.base + "/authentication/session" + Endpoints.apiKeyParam
             }
         }
         
